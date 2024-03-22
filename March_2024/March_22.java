61816d319e84546dcbe5071dec2ff15f89424aa4
AakashAkshat <aakashakshat2002@gmail.com>
2024-03-22 20:08:06 +0530

    22nd March GFG POTD Solution

---
+++ March_2024/March_22.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.w3c.dom.Node;
+
+public class March_22 {
+    class Tree {
+    public static void diagonalSumUtil(Node root, int diagonal, Map<Integer, Integer> map)
+    {
+        // base case: empty tree
+        if (root == null)
+            return;
+
+        map.put(diagonal, map.getOrDefault(diagonal, 0) + root.data);
+        diagonalSumUtil(root.left, diagonal + 1, map);
+        diagonalSumUtil(root.right, diagonal, map);
+    }
+
+    public static ArrayList <Integer> diagonalSum(Node root)
+    {
+        ArrayList <Integer> res = new ArrayList <Integer> (0);
+        Map<Integer, Integer> map = new HashMap<>();
+        diagonalSumUtil(root, 0, map);
+
+        for (Integer key : map.values())
+            res.add(key);
+
+        return res;
+    }
+    
+}
+    
+}
