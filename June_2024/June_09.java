7749a84b2836443d4c4c36916b60eeaaabb9faea
AakashAkshat <aakashakshat2002@gmail.com>
2024-06-09 21:44:51 +0530

    09th June GFG POTD Solution

---
+++ June_2024/June_09.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static void zigZag(int n, int[] arr) {
+        for (int i = 0; i < n - 1; ++i) {
+            if ((i % 2 == 0 && arr[i] > arr[i + 1]) || (i % 2 == 1 && arr[i] < arr[i + 1])) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+    }
+}
